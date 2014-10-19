7680cbd3d411b6dc821261dfa81e062a4e85a975
Lorenzo De Leon Alipio <Lorenzo-D-Alipio@users.noreply.github.com>
2014-10-19 00:50:54 -0700

    Create tbs.php
    added tbs.php configuration file for the tbs library

---
+++ application/config/tbs.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+
+$config = array(
+				'left_delimiter' => '{% ',
+				'right_delimiter' => ' %}',
+				'theme_path_tbs' => APPPATH.'views/'
+);
